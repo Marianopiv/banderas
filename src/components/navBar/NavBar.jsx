0e6a07fa7ce767e1ded0f43b67e791e428553a3c
Marianopiv <marianopividori93@gmail.com>
2022-11-20 15:00:10 -0300

    new version

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -9,7 +9,8 @@ const NavBar = () => {
     <div onClick={toogleDarkMode} className={`flex justify-between gap-5 ${darkMode?"bg-slate-700 text-white":"bg-white"} px-3 py-8 hover:cursor-pointer`} >
       <div className=""><h3 className="text-sm font-bold">Where in the world?</h3></div>
       <div className="flex gap-2"><img className={`w-4 h-4 -rotate-45 ${darkMode?"invert":""}`} src={moon} alt="" />
-      <h3 className="text-xs font-bold">Dark Mode</h3></div>
+      <h3 className="text-xs font-bold">Dark Mode</h3>
+      <p className="text-xs">V 2.0</p></div>
     </div>
   );
 };
