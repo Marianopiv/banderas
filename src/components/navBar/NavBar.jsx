a73501420f5d8ad196bf9085adcefd2840771f3e
Marianopiv <marianopividori93@gmail.com>
2023-01-22 16:27:56 -0300

    aa

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
   const {toogleDarkMode,darkMode } =
     useContext(FlagProvContext);
   return (
-    <div  className={`flex justify-between gap-5 ${darkMode?"bg-slate-700 text-white":"bg-white"} px-3 py-8 `} >
+    <div  className={`flex justify-between gap-5 ${darkMode?"bg-slate-700 text-white":"bg-white dark:text-black"} px-3 py-8 `} >
       <div className=""><h3 className="text-sm font-bold">Where in the world?</h3></div>
       <div onClick={toogleDarkMode} className="flex gap-2 hover:cursor-pointer"><img className={`w-4 h-4 -rotate-45 ${darkMode?"invert":""}`} src={moon} alt="" />
       <h3 className="text-xs font-bold">Dark Mode</h3>
